709791e929d3db631c134fb536b7717645734751
francois <>
2021-04-03 19:36:29 -0400

    updated __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 #!usr/bin/env/python3
 
+# __init__.py
+
 from .getnametuple import getnametuple
 from .get_ntpl import get_ntpl
 from .zscanner import zscanner
 from .save_archv import save_archv
 from .xtrct_archv import xtct_archv
+from .scanzip.scanzip import scanzip
+
