8f320476ae70af26aa21bf167a4f7ab23c2b2468
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-10-26 16:09:37 -0400

    Update save_archv.py
    Unnecessary

--- save_archv.py
+++ save_archv.py
@@ -1,27 +1,27 @@
 #!usr/bin/env/python3
 
-''' Could be made independant from pandas '''
+# ''' Could be made independant from pandas '''
 
-import os
-from typing import Union
-import pandas as pd
-from loadutils.get_dst_path import get_dst_path
-from loadutils.filterlist_inc import filterlist_inc
-from sniffbytes.stream2file import stream2file
+# import os
+# from typing import Union
+# import pandas as pd
+# from loadutils.get_dst_path import get_dst_path
+# from loadutils.filterlist_inc import filterlist_inc
+# from sniffbytes.stream2file import stream2file
 
-def save_archv(
-    vals: pd.DataFrame,
-    to_xtrct: Union[str, list, tuple],
-    dst_path: Union[str, os.PathLike]
-) -> None:
-    """ Writes unzipped file buffers to disk. """
-    [stream2file(row[1].bsheets,
-                 os.path.join(get_dst_path(dst_path),
-                       os.path.basename(row[1].filename).lower()))
-     for row in vals.iterrows() if row[1].src_names in
-     tqdm(filter_lst_inc(to_xtrct, vals.src_names),
-          "extracting archive")]
+# def save_archv(
+#     vals: pd.DataFrame,
+#     to_xtrct: Union[str, list, tuple],
+#     dst_path: Union[str, os.PathLike]
+# ) -> None:
+#     """ Writes unzipped file buffers to disk. """
+#     [stream2file(row[1].bsheets,
+#                  os.path.join(get_dst_path(dst_path),
+#                        os.path.basename(row[1].filename).lower()))
+#      for row in vals.iterrows() if row[1].src_names in
+#      tqdm(filter_lst_inc(to_xtrct, vals.src_names),
+#           "extracting archive")]
 
-def main():
-    if __name__ == __main__:
-        save_archv(vals, to_xtrct, dst_path)
+# def main():
+#     if __name__ == __main__:
+#         save_archv(vals, to_xtrct, dst_path)
