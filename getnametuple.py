c398dd50e673f369657b405e5cefc790816ca6fd
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-10-26 16:01:59 -0400

    Update getnametuple.py
    Type hints and docstring update

--- getnametuple.py
+++ getnametuple.py
@@ -4,20 +4,24 @@ import os
 from typing import Union
 from zipfile import ZipFile
 
-def getnametuple(myzip):
-    """
-    Adjustment to ZipFile.namelist() function to prevent MAC-exclusive
-    '__MACOSX' and '.DS_Store' files from interfering.
+def getnametuple(myzip: zipfile.ZipFile) -> tuple:
+    """ Adjustment to ZipFile.namelist() method.
+    
+    Alternative to ZipFile.namelist() preventing MAC-exclusive
+    '__MACOSX', '.DS_Store', etc. files from interfering.
     Only necessary for files compressed with OS 10.3 or earlier.
     Source: https://superuser.com/questions/104500/what-is-macosx-folder
     Command line solution:
         ``` zip -r dir.zip . -x ".*" -x "__MACOSX"
     Source: https://apple.stackexchange.com/questions/239578/compress-without-ds-store-and-macosx
-
-    Parameters
-    ----------
-    myzip: ZipFile object
+    
+    Args:
+        myzip: ZipFile object
+    
+    Returns:
+        Tuple containing a zip file member names
     """
+
     return tuple(
         sorted(
             list(
