45051e6c9521178d466900f89b71527696e75984
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-10-27 16:14:08 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,9 +3,5 @@
 # __init__.py
 
 from .getnametuple import getnametuple
-# from .get_ntpl import get_ntpl
 from .zscanner import zscanner
-# from .save_archv import save_archv
-# from .xtrct_archv import xtrct_archv
 from .scan_zip import scan_zip
-
